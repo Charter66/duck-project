1feaad401ffac00fb3ee326a2917954da083cc0a
Lazar Godjirov <lazargodzirov@yahoo.com>
2023-05-09 18:55:29 +0200

    added CORS

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
+const cors = require('cors');
+
 const express = require('express');
 const bodyParser = require('body-parser'); // Add this line
 const app = express();
 const port = process.env.PORT || 8000;
 const router = require('./routes/index');
 const errorHandler = require('./middleware/errorHandler')
+app.use(cors());
 
 app.use(bodyParser.json()); // Add this line to parse JSON request bodies
 app.use('/', router);
