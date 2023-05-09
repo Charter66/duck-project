54958fab1eef0f45ce5623828f764eb92769f448
Lazar Godjirov <lazargodzirov@yahoo.com>
2023-05-09 19:24:22 +0200

    hhmm

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,17 @@
 require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
-const bodyParser = require('body-parser'); // Add this line
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 8000;
 const router = require('./routes/index');
-const errorHandler = require('./middleware/errorHandler')
+const errorHandler = require('./middleware/errorHandler');
+
 app.use(cors());
-
-app.use(bodyParser.json()); // Add this line to parse JSON request bodies
+app.use(bodyParser.json());
 app.use('/', router);
-app.use(express.json());
+app.use(errorHandler);
 
-app.use(errorHandler)
 app.listen(port, () => {
   console.log(`Server up on port ${port}`);
 });
